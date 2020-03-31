0fe056aa10352365d828f16f5cc571c3d7324b99
carlosMoreno357 <carlos.moreno.salazar@gmail.com>
2020-03-31 09:50:49 -0600

    [ADD] sidekiq adapter to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,10 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  #sidekiq adapter
+  config.active_job.queue_adapter = :sidekiq
+
+
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
