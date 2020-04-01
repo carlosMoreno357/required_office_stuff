71c921f0c74d7cde6fe442c9884d3a75a7961345
carlosMoreno357 <carlos.moreno.salazar@gmail.com>
2020-04-01 13:23:16 -0600

    [ref] refactored items model test

--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 class ItemTest < ActiveSupport::TestCase
   test 'item has a valid description, category, status and belongs to a user' do
     user = User.create(email: "test@testmail.com", password: "test_password")
-    item=Item.create(description: 'valid_description', category_id: 1, status: 1, user_id: user.id )
+    category = User.create(category_name: "TEST CATEGORY", colour: "#498467")
+    item=Item.create(description: 'valid_description', category_id: category.id, status: 1, user_id: user.id )
     assert item.valid?
   end
 
@@ -13,7 +14,7 @@ class ItemTest < ActiveSupport::TestCase
 
   test 'item updated with a valid comment' do
     item=Item.create(description: 'valid_description', category_id: 1, status: 1, user_id: 1)
-    update = item.update(comment:'test comment')
+    update = item.update(details:'test comment')
     assert update_truthy
   end
 end
