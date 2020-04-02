742cfca25263c349465ad75bc57bddb4cb97f11a
carlosMoreno357 <carlos.moreno.salazar@gmail.com>
2020-04-01 22:01:24 -0600

    [WIP]adds delays to -Item status can be changed to dismissed by the service - for http requests[D

--- test/services/change_status_service_test.rb
+++ test/services/change_status_service_test.rb
@@ -11,7 +11,9 @@ class ChangeStatusServiceTest < ActiveSupport::TestCase
 
   test 'Item status can be changed to dismissed by the service' do
     user = User.create(email: "test@testmail.com", password: "test_password", admin: true)
+    sleep 1.5
     item=Item.create(description: 'valid_description', category_id: 1, status: 1, user_id: user.id )
+    sleep 1.5
     response = ChangeStatusService.set(2,item.id,user)
     assert response
   end
