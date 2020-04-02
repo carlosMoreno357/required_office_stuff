a117a14943bd2334053e10b59c3e52c0bbbae813
carlosMoreno357 <carlos.moreno.salazar@gmail.com>
2020-04-01 22:35:58 -0600

    [WIP] email has been sent to user when an item gets commented - delays

--- test/mailers/stuff_mailer_test.rb
+++ test/mailers/stuff_mailer_test.rb
@@ -22,8 +22,12 @@ class StuffMailerTest < ActionMailer::TestCase
   end
 
   test "email has been sent to user when an item gets commented" do
+    @category = Category.create(category_name:'test category', colour: "#F02D3A")
+    sleep 1.5
     @user = User.create(email: 'carlos.moreno@tangosource.com', password: '1234')
-    @item=Item.create(description: 'valid_description', category_id: 1, status: 1, user_id: @user.id)
+    sleep 1.5
+    @item=Item.create(description: 'valid_description', category_id: @category.id, status: 1, user_id: @user.id)
+    sleep 1.5
     @comment = Comment.create(description: "desc", item_id: @item.id, user_id: @user.id)
 
     email = StuffMailer.comment_email(@user,@item).deliver_now
