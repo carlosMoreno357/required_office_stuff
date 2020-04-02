3943a8249542cf959f4cbd9ff3f76f4525d02540
carlosMoreno357 <carlos.moreno.salazar@gmail.com>
2020-04-01 22:44:33 -0600

    [WIP] email has been sent to administrators when a new item is requested - delays

--- test/mailers/stuff_mailer_test.rb
+++ test/mailers/stuff_mailer_test.rb
@@ -3,10 +3,14 @@ require 'test_helper'
 class StuffMailerTest < ActionMailer::TestCase
 
   test "email has been sent to administrators when a new item is requested" do
+    @category = Category.create(category_name:'test category', colour: "#F02D3A")
+    sleep 1.5
     @admin = User.create(email: 'carlos.moreno.salazar@gmail.com', password: '1234', admin:true)
+    sleep 1.5
     @user = User.create(email: 'carlos.moreno@tangosource.com', password: '1234')
+    sleep 1.5
     @item=Item.create(description: 'valid_description', category_id: 1, status: 1, user_id: @user.id)
-
+    sleep 1.5
     email = StuffMailer.new_requirement_email(@user,@admin.email,@item).deliver_now
     assert_emails 1
 
