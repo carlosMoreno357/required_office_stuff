3c2b63f5877cd9f220b3661085ddcd7bbcaf800b
carlosMoreno357 <carlos.moreno.salazar@gmail.com>
2020-04-01 18:22:39 -0600

    [WIP] Item model update test fix

--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -14,7 +14,7 @@ class ItemTest < ActiveSupport::TestCase
 
   test 'item updated with a valid comment' do
     item=Item.create(description: 'valid_description', category_id: 1, status: 1, user_id: 1)
-    update = Item.update(details:'test comment')
+    update = Item.last.update(details:'test comment')
     assert update
   end
 end
