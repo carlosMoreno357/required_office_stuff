04297c9ebfe303db3a5d85e2645171d2c3884668
carlosMoreno357 <carlos.moreno.salazar@gmail.com>
2020-04-01 22:54:24 -0600

    [WIP] Item status can be changed to dismissed by the service - delays

--- test/services/change_status_service_test.rb
+++ test/services/change_status_service_test.rb
@@ -12,9 +12,11 @@ class ChangeStatusServiceTest < ActiveSupport::TestCase
   end
 
   test 'Item status can be changed to dismissed by the service' do
+    @category = Category.create(category_name:'test category', colour: "#F02D3A")
+    sleep 1.5
     user = User.create(email: "test@testmail.com", password: "test_password", admin: true)
     sleep 1.5
-    item=Item.create(description: 'valid_description', category_id: 1, status: 1, user_id: user.id )
+    item=Item.create(description: 'valid_description', category_id: @category.id, status: 1, user_id: user.id )
     sleep 1.5
     response = ChangeStatusService.set(2,item.id,user)
     assert response
